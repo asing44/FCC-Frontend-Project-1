9540237a6cb9a5d101ebc851257f395616dc31e9
asing44 <adamwarsing@gmail.com>
2024-01-06 15:20:52 -0500

    Bug Fixing

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  base: "https://asing44.github.io/",
+  root: "./",
   build: {
     outDir: '../docs',
   },
