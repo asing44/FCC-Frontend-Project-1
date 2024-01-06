6b9cbf493393a620fdd3005ca4184ee20c153ec1
asing44 <adamwarsing@gmail.com>
2024-01-06 15:31:26 -0500

    Created Page Build Action

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -5,8 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  root: "./",
-  build: {
-    outDir: '../docs',
-  },
+  base: "/FCC Frontend Project 1/",
 })
